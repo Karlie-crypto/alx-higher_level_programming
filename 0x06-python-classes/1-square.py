0bf923c295be8e8823cb11a05cfb7bd970c90570
Karlie-crypto <karlieemoyo@gmail.com>
2022-12-20 05:30:22 -0800

    Python Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines a class named Square """
+
+
+class Square:
+    """ defines a function called __init__ """
+    def __init__(self, size):
+        """ initializes __size of self with size """
+        self.__size = size
