534158c90e46e692d6a2d95227e34a9d888212cd
Karlie-crypto <karlieemoyo@gmail.com>
2023-01-03 02:40:56 -0800

    Python More Classes and Objects

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Defines Rectangle class
+"""
+
+
+class Rectangle:
+    """ Rectangle """
+    def __init__(self, width=0, height=0):
+        """ initizie width and height """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
