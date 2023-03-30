81b7e3b1c77d697dbe9e58408e940700262559be
karlie moyo <karlieemoyo@gmail.com>
2023-03-30 17:56:27 +0200

    Takes in Url as an argment

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# send a GET request to the URL using curl, and display the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
